f00c0866e97bc8e55b9707de99f2bf204b5994f3
SainiShivanshu <shivanshusaini216@gmail.com>
2022-09-17 11:06:15 +0530

    version 1.1

--- app/src/main/java/com/example/whatsappclone/model/UserModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.whatsappclone.model
-
-data class UserModel(
-    val uid:String?="",
-            val name :String?="",
-                    val number:String?="",
-                            val imageUrl:String?=""
-)
